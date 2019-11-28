884edc04ec5d0ff27d7eb00da6f5c8038f863564
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2019-11-28 23:48:37 +0900

    Supported operator>.

--- include/fixedpointnumber.h
+++ include/fixedpointnumber.h
@@ -138,6 +138,17 @@ class fixed_t {
     return lhs.fixed_point_ == rhs.fixed_point_;
   }
 
+  /// Comparison operator, greater than.
+  ///
+  /// @param[in] lhs Left hand side value which is compared to
+  /// @param[in] rhs Right hand side value which is compared to
+  ///
+  /// @retval true  lhs is greater than rhs
+  /// @retval false lhs is not greater than rhs
+  friend bool operator>(const fixed_t& lhs, const fixed_t& rhs) {
+    return lhs.fixed_point_ > rhs.fixed_point_;
+  }
+
   /// Get holding fixed point value as string.
   ///
   /// @return Holding fixed point value as string.
---
+++ test/test_comparison_gt.cc
@@ -0,0 +1,40 @@
+//
+// Copyright 2019 Minoru Sekine
+//
+// This file is part of libfixedpointnumber.
+//
+// libfixedpointnumber is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// libfixedpointnumber is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with libfixedpointnumber.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <cstdint>
+
+#include <gtest/gtest.h>
+
+#include "fixedpointnumber.h"
+
+using fixed_t = fixedpointnumber::fixed_t<int16_t, 5>;
+
+class ComparisonGtTest
+  : public ::testing::TestWithParam<double> {
+};
+
+TEST_P(ComparisonGtTest, Gt) {
+  const fixed_t param(GetParam());
+  fixed_t incremented_param(param);
+  ++incremented_param;
+  EXPECT_GT(incremented_param, param);
+}
+
+INSTANTIATE_TEST_SUITE_P(Instance0,
+                         ComparisonGtTest,
+                         ::testing::Range(-2.125, 3.0, 0.5));
