495a2f3d28de16d620bfc23c65f417328cc9ab98
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2020-08-20 21:37:11 +0900

    Added fixed_signbit() to validate value is negative or not.

---
+++ include/fixedpointnumber_math_signbit-priv.h
@@ -0,0 +1,46 @@
+//
+// Copyright 2020 Minoru Sekine
+//
+// This file is part of libfixedpointnumber.
+//
+// libfixedpointnumber is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// libfixedpointnumber is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with libfixedpointnumber.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef INCLUDE_FIXEDPOINTNUMBER_MATH_SIGNBIT_PRIV_H_
+#define INCLUDE_FIXEDPOINTNUMBER_MATH_SIGNBIT_PRIV_H_
+
+#include "fixedpointnumber.h"
+
+#ifndef FIXEDPOINTNUMBER_MATH_INTERNAL
+#error Do not include this file directly, ixedpointnumber_math.h instead.
+#endif
+
+namespace fixedpointnumber {
+
+/// Validate the value is negative or not.
+///
+/// @tparam IntType Internal int type for type fixed_t template param
+/// @tparam Q       Q for type fixed_t template param
+///
+/// @param src value to compute absolute value
+///
+/// @retval true  src is positive or zero
+/// @retval false src is negative
+template <typename IntType, std::size_t Q>
+constexpr bool fixed_signbit(fixed_t<IntType, Q> src) {
+  return (src.fixed_point_ < 0);
+}
+
+}  // namespace fixedpointnumber
+
+#endif  // INCLUDE_FIXEDPOINTNUMBER_MATH_SIGNBIT_PRIV_H_
