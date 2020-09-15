6ed78b9c6c6d1b2617f9aec51281c9573f42a2dc
MinoruSekine <MinoruSekine@users.noreply.github.com>
2020-09-15 10:56:26 +0000

    deploy: 5f590c94497f20a2288e05e67bb44e3372702062

--- Doxygen/html/dir_d44c64559bbebec7f509842c48db8b23.js
+++ Doxygen/html/dir_d44c64559bbebec7f509842c48db8b23.js
@@ -9,6 +9,7 @@ var dir_d44c64559bbebec7f509842c48db8b23 =
     [ "fixedpointnumber_math_abs-priv.h", "fixedpointnumber__math__abs-priv_8h_source.html", null ],
     [ "fixedpointnumber_math_copysign-priv.h", "fixedpointnumber__math__copysign-priv_8h_source.html", null ],
     [ "fixedpointnumber_math_mod-priv.h", "fixedpointnumber__math__mod-priv_8h_source.html", null ],
+    [ "fixedpointnumber_math_modf-priv.h", "fixedpointnumber__math__modf-priv_8h_source.html", null ],
     [ "fixedpointnumber_math_round-priv.h", "fixedpointnumber__math__round-priv_8h_source.html", null ],
     [ "fixedpointnumber_math_signbit-priv.h", "fixedpointnumber__math__signbit-priv_8h_source.html", null ],
     [ "fixedpointnumber_stream-priv.h", "fixedpointnumber__stream-priv_8h_source.html", null ],
