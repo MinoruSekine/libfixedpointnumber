672a214a7ea2102993db609f34ae9c19643b1f93
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2021-02-10 22:45:44 +0900

    Support pre-compiled header for test on Windows Visual Studio.

---
+++ test/gtest_compat.cc
@@ -0,0 +1,21 @@
+//
+// Copyright 2021 Minoru Sekine
+//
+// This file is part of libfixedpointnumber.
+//
+// libfixedpointnumber is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// libfixedpointnumber is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with libfixedpointnumber.  If not, see <http://www.gnu.org/licenses/>.
+
+// This file is for making pre-compiled header on Visual Studio.
+
+#include "gtest_compat.h"
